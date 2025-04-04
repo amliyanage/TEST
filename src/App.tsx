0f1999001aacd89878f6f6aff412827f28549832
Ashen Madhushanka <161613675+amliyanage@users.noreply.github.com>
2025-04-04 09:20:38 +0530

    get users functionality with API integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { User } from './model/User';
+import UserService from './Services/UserService';
 
 function App() {
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await UserService.getUsers();
+      setUsers(response.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Weavy User Management</h2>
+
+      <h3>Users List</h3>
+      <ul>
+        {Array.isArray(users) && users.length > 0 ? (
+          users.map((user: any) => (
+            <li key={user.id}>
+              {user.name} - {user.email}
+            </li>
+              ))
+            ) : (
+              <li>No users found.</li>
+          )}
+      </ul>
     </div>
   );
 }
---
+++ src/Services/UserService.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { User } from "../model/User";
+
+const API_URL = "https://8015b5dbc0724d38882ac90397c27649.weavy.io/api/users";
+const API_KEY = "wys_hMWpXdekxcn9Gc8Ioah3azOllzUZ7l3HN9yB";
+
+const headers = {
+  Authorization: `Bearer ${API_KEY}`,
+  "Content-Type": "application/json",
+};
+
+const UserService = {
+  getUsers: () => axios.get<User[]>(API_URL, { headers }),
+};
+
+export default UserService;
